74ba78c82f02e1428f7f9b0c1617941ea443bc76
irrarragorridayron <irrarragorridayron@gmail.com>
2026-08-14 22:18:45 -0400

    Add files via upload
    Publicar Italy Mau Producer PWA

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "italy-mau-producer-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./media/logo.png",
+  "./media/antes_de_olvidarte.png",
+  "./media/brutal.png",
+  "./media/antes_de_olvidarte.mp3",
+  "./media/brutal.mp3"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      });
+    })
+  );
+});
